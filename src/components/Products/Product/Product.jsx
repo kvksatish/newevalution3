a30e045116a413ce0d8f06f54bd787f08b15643c
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-06-06 11:42:10 +0530

    added required key

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 const Product = () => {
+  // Note: this id should come from api
+  const product = { id: 1 };
   return (
-    <div data-cy="product">
+    <div data-cy={`product-${product.id}`}>
       <h3 data-cy="product-name"></h3>
       <h6 data-cy="product-description"></h6>
       <button data-cy="product-add-item-to-cart-button"></button>
